f0d4a2ec11b85bed09729473948eecadba99d573
Kelley Campbell IV <132499829+SupVSSL7@users.noreply.github.com>
2025-06-09 03:33:18 -0400

    A few exercises from John Elder to get the coding juices going

---
+++ main.py
@@ -0,0 +1,23 @@
+# reinforcing foundations along with newly acquired security skills and awareness
+# to produce new solutions from previous methods.
+
+
+num1 = input('Enter a number: ')
+num2 = input('Enter a second number: ')
+answ = int(num1) + int(num2)
+print(f'{num1} + {num2} = {int(num1) + int(num2)}')
+print(answ)
+
+#####
+
+me = "Kelley"
+
+if me:
+    print(me + " is Here!!")
+    print(me * 10)
+
+
+#####
+first = input("whats your first name? ")
+last  = input("whats your last name?")
+print(first +  last, " it's nice to meet you!")
