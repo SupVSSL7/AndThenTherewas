46e7cbf4e3fdaa6aa1e422afac40394a4a9f8938
Kelley Campbell IV <132499829+SupVSSL7@users.noreply.github.com>
2025-06-30 03:02:04 -0400

    A few exercises from John Elder: drilling foundations if/elif/else

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # reinforcing foundations along with newly acquired security skills and awareness
 # to produce new solutions from previous methods.
 
-
+'''
 num1 = input('Enter a number: ')
 num2 = input('Enter a second number: ')
 answ = int(num1) + int(num2)
@@ -19,14 +19,47 @@ if me:
 
 #####
 first = input("whats your first name? ")
-last  = input("whats your last name?")
+last  = input("whats your last name? ")
 print(first +  last, " it's nice to meet you!")
 
 email = input("Enter your email:  ")
 city  = input("Enter your city:  ")
 phone = input("Enter phone: ")
 
-print("Does this look correct?:")
 print(first +  last, city)
 print(email, phone)
+# finalprint = input(bool("Does this look correct?: "))
+#if finalprint == yes:
+#    print("Great!")
+#else:
+#   print("just start over again, please..")'''
+
+x=42
+if x==41:
+    print(" X does in fact equal 42")
+
+x = 42
+if x == 42:
+    print(f'{int(x)} does in FACT equal X')
+else:
+    print("x does NOT equal 42")
+
+name = input("what is your name: ")
+if name == "holly":
+    print("Hey " + name + " BABY!!")
+elif name == "kelley":
+    print("AY " + name + " THATS MY DAWG!")
+else:
+    print("WHO ARE YOU again BRO?")
+
+name = "holly"
+if name == "holly" or name == "kelley":
+    print("THERES THAT HOT COUPLE!!")
+else:
+    print("You're not Holly or Kelley")
+
+
+
+
+
 
